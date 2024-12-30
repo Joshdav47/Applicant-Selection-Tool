a678fa8848065db1fc825370ebb7e7fca49601e7
Joshua Davis <167588957+Joshdav47@users.noreply.github.com>
2024-12-30 15:51:15 -0600

    Add files via upload

--- Excel-erate_chooser.py
+++ Excel-erate_chooser.py
@@ -1,5 +1,4 @@
 import csv
-
 import pandas as pd
 
 # Read CSV file and created a dataframe
@@ -16,17 +15,15 @@ def applicantaccepter(target_size):
 
     # Create priority lists and accepted applicants
     acceptedPatrons = {}
-    waitlistPatrons = {}
     income= {}
     education = {}
     identity = {}
-
-    # TODO: Implement waitlist
 
     for i in range(len(df)):
         # Accepts all
         if len(df) <= target_size:
             acceptedPatrons.update({df.iloc[i, 2]: df.iloc[i, 1]})
+            return acceptedPatrons
 
         elif ((df.iloc[i, 3] == 'Yes' and df.iloc[i, 5] == 'Yes' and df.iloc[i, 2] not in acceptedPatrons
                 and not pd.isnull(df.iloc[i, 1])) and df.iloc[i, 6]
@@ -39,59 +36,21 @@ def applicantaccepter(target_size):
                   df.iloc[i, 11] == 'High School/GED' or df.iloc[i, 11] == 'Some College but no degree'):
                 education.update({df.iloc[i, 2]: df.iloc[i, 1]})
 
-            elif (df.iloc[i, 11] == 'Female' or df.iloc[i, 11] == 'Non-binary/third gender' or
-                    df.iloc[i, 11] == 'Cisgender' or df.iloc[i, 11] == 'Agender' or df.iloc[i, 11] == 'Genderqueer' or
+            elif (df.iloc[i, 10] == 'Female' or df.iloc[i, 10] == 'Non-binary/third gender' or
+                    df.iloc[i, 10] == 'Cisgender' or df.iloc[i, 10] == 'Agender' or df.iloc[i, 10] == 'Genderqueer' or
                     df.iloc[i, 9] == 'Asian' or df.iloc[i, 9] == 'Black, non-Hispanic' or df.iloc[i, 9] == 'Hispanic/Latino'
                     or df.iloc[i, 9] == 'Native Hawaiian/Pacific Islander' or df.iloc[i, 9] == 'Native American'):
                 identity.update({df.iloc[i, 2] : df.iloc[i, 1]})
 
-    if (len(income) + len(education) + len(identity) < target_size) or len(df) <= target_size:
-        for name, email in income.items():
-                acceptedPatrons[name] = email
-        for name, email in education.items():
-                acceptedPatrons[name] = email
-        for name, email in identity.items():
-                acceptedPatrons[name] = email
-    else:
-        while len(acceptedPatrons) < target_size:
-            for name, email in income.items():
-                    acceptedPatrons[name] = email
-            for name, email in education.items():
-                    acceptedPatrons[name] = email
-            for name, email in identity.items():
-                    acceptedPatrons[name] = email
+    acceptedPatrons = income | education | identity
 
-    accepted_emails = set(acceptedPatrons.values())
-    while len(waitlistPatrons) < target_size:
-        for name, email in income.items():
-            if name not in acceptedPatrons and email not in accepted_emails:
-                waitlistPatrons[name] = email
-        for name, email in education.items():
-            if name not in acceptedPatrons and email not in accepted_emails:
-                waitlistPatrons[name] = email
-        for name, email in identity.items():
-            if name not in acceptedPatrons and email not in accepted_emails:
-                waitlistPatrons[name] = email
+    if len(acceptedPatrons) < target_size:
+        for i in range(len(df)):
+            while len(df) <= target_size:
+                if df.iloc[i, 2] not in acceptedPatrons:
+                    acceptedPatrons.update({df.iloc[i, 2]: df.iloc[i, 1]})
 
-    if target_size < len(acceptedPatrons):
-        acceptedPatrons = trim_dictionary(acceptedPatrons, target_size)
-    if target_size < len(waitlistPatrons):
-        waitlistPatrons = trim_dictionary(waitlistPatrons, target_size)
-
-    # if target_size > len(acceptedPatrons):
-    #     while len(acceptedPatrons) < target_size:
-    #             for name, email in income.items():
-    #                 if name not in acceptedPatrons and email not in acceptedPatrons:
-    #                     acceptedPatrons[name] = email
-    #             for name, email in education.items():
-    #                 if name not in acceptedPatrons and email not in acceptedPatrons:
-    #                     acceptedPatrons[name] = email
-    #             for name, email in identity.items():
-    #                 if name not in acceptedPatrons and email not in acceptedPatrons:
-    #                     acceptedPatrons[name] = email
-
-
-    return acceptedPatrons, waitlistPatrons
+    return acceptedPatrons
 
 def main():
     # TODO: have the input checked for any letters or symbols, and removed
@@ -100,14 +59,21 @@ def main():
         while target_size < 1 or target_size > len(df):
             target_size = int(input("Please enter a valid size: "))
 
+    acceptedPatrons = applicantaccepter(target_size)
+    print(len(acceptedPatrons))
+    # with open('acceptedPatrons.csv', 'w', newline='') as file:
+    #     writer = csv.writer(file)
+    #     writer.writerow(['Name', 'Email'])
+    #     for name, email in acceptedPatrons.items():
+    #         writer.writerow([name, email])
 
-    acceptedPatrons, waitlistedPatrons = applicantaccepter(target_size)
-    with open('acceptedPatrons.csv', 'w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(['Name', 'Email', 'Waitlisted Name', 'Waitlisted Email'])
-        for name, email in acceptedPatrons.items():
-            for waitlistedName, waitlistedEmail in waitlistedPatrons.items():
-                writer.writerow([name, email, waitlistedName, waitlistedEmail])
+    # acceptedPatrons, waitlistedPatrons = applicantaccepter(target_size)
+    # with open('acceptedPatrons.csv', 'w', newline='') as file:
+    #     writer = csv.writer(file)
+    #     writer.writerow(['Name', 'Email', 'Waitlisted Name', 'Waitlisted Email'])
+    #     for name, email in acceptedPatrons.items():
+    #         for waitlistedName, waitlistedEmail in waitlistedPatrons.items():
+    #             writer.writerow([name, email, waitlistedName, waitlistedEmail])
 
 
 if __name__ == '__main__':
